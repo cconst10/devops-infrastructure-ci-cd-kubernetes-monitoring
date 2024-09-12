e8c05c2c547168fe2eaa7713f8c49d2ebf468c5e
cconst10 <71453929+cconst10@users.noreply.github.com>
2024-09-12 21:47:41 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@
  } 
 
  resource "aws_instance" "example" {
-        ami = "ami-02ffde950ebf433fd" #AMI ID for EC2 instance - Amazon Linux 2
+        ami = "ami-02ffde950ebf433fd" #AMI ID for EC2 instance
         instance_type = "t3.micro"
         tags = {
           Name = "example-instance"
