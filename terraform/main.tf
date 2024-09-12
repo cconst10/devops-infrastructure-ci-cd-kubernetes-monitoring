72628898c0eb7bfd6d7a8bd982116c168dc60533
cconst10 <cconst10@ucy.ac.cy>
2024-09-12 21:45:47 +0300

    terraform main.tf added

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+# main.tf
+
+ provider "aws" {
+        region = "eu-north-1" #AWS region
+ } 
+
+ resource "aws_instance" "example" {
+        ami = "ami-02ffde950ebf433fd" #AMI ID for EC2 instance - Amazon Linux 2
+        instance_type = "t3.micro"
+        tags = {
+          Name = "example-instance"
+        }
+ } 
+
