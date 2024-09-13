88c38ef607630d0debc75a0b3c5af3bf40c87cb1
cconst10 <cconst10@ucy.ac.cy>
2024-09-13 18:58:21 +0300

    aws key var fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
-# terraform/main.tf
-
 provider "aws" {
-  region = var.region
+  region     = var.region
+  access_key = var.AWS_ACCESS_KEY_ID   # Added access_key to fetch from environment
+  secret_key = var.AWS_SECRET_ACCESS_KEY  # Added secret_key to fetch from environment
 }
 
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
   tags = {
-    Name = "example-instance" #test commit tf pipeline
+    Name = "example-instance" 
   }
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,3 @@
-# terraform/variables.tf
-
 variable "region" {
   description = "The AWS region to create resources in."
   type        = string
@@ -9,12 +7,22 @@ variable "region" {
 variable "ami_id" {
   description = "The AMI ID for the EC2 instance."
   type        = string
-  default     = "ami-02ffde950ebf433fd" # change AMI ID to use preffered AMI  
+  default     = "ami-02ffde950ebf433fd" # Change to the preferred AMI
 }
 
 variable "instance_type" {
   description = "The type of EC2 instance."
   type        = string
   default     = "t3.micro"
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS access key ID"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS secret access key"
+  type        = string
 }
 
